490738055611503c590061e74cad3e1178578981
Mausmato <Singeripad2@gmail.com>
2024-06-07 22:27:43 -0400

    feat(all): fixed the src

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import './App.css' 
+import BackgroundGradientAnimation from "./components/bg"
 
 function App() {
   return (
     <>
-    <p className="text-red-500">hello</p>
+      <BackgroundGradientAnimation>
+        <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+          <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+            Gradients X Animations
+          </p>
+        </div>
+      </BackgroundGradientAnimation>
+      <div className="flex-row">
+        <div className="text-red-400">
+          <h1>Hello World</h1>
+          <h1>Hello World</h1>
+          <h1>Hello World</h1>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
